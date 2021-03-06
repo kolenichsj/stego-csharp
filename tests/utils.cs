f9b454771f0b426dd5cfd07b22f1125dfb98b2e2
Steve Kolenich <kolenichsj@tutanota.com>
2021-03-06 11:44:00 -0500

    Move compare code method to Utils class

---
+++ tests/utils.cs
@@ -0,0 +1,27 @@
+using System.IO;
+
+public static class utils
+{
+
+
+        /// <summary>
+        /// Executes binary compares of two file streams
+        /// </summary>
+        /// <param name="fs1"></param>
+        /// <param name="fs2"></param>
+        /// <returns>True if the files are binary identical</returns>
+        public static bool FileCompare(FileStream fs1, FileStream fs2)
+        {
+            // Check the file sizes. If they are not the same, the files are not the same.
+            if (fs1.Length == fs2.Length)
+            {
+                // Compare each byte from each file until either a non-matching set
+                // of bytes is found or the end of file1 is reached.
+                do { } while ((fs1.ReadByte() == fs2.ReadByte()) && (fs1.Position != fs1.Length));
+
+                return (fs1.Position == fs1.Length); //if end of file is reached, files were equal
+            }
+
+            return false; // Return false to indicate files are different
+        }
+}
